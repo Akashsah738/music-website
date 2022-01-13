e365e6ed83c1f3ff68d8302f8ee72a4888f6f59c
akash <akashsah738@gmail.com>
2022-01-13 15:38:44 +0545

    adding toglebar

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ $(document).ready(function() {
     })
 
 })
+
+
+function onClickMenu() {
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("nav").classList.toggle("change");
+    // alert('click here');
+}
